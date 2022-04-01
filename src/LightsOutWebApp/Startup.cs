2f80bf23eca09e3c8f23ba0b8676143299045e85
adrianvellamlt <adrian.vella@outlook.com>
2022-04-01 07:49:58 +0200

    Initial implementation of game

--- src/LightsOutWebApp/Startup.cs
+++ src/LightsOutWebApp/Startup.cs
@@ -1,5 +1,8 @@
+using System;
+using System.Text.RegularExpressions;
 using HashidsNet;
 using LightsOut.GameLogic;
+using LightsOut.Infrastructure;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -21,6 +24,8 @@ namespace LightsOut.Web
 
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<ISystemClock, RealSystemClock>();
+
             services.AddCaching();
 
             services.AddSingleton<IHashids>(_ => new Hashids(Configuration.GetValue<string>("Hashids:Salt")));
@@ -52,35 +57,41 @@ namespace LightsOut.Web
                     await context.Response.WriteAsync("pong", context.RequestAborted);
                 });
 
+                endpoints.MapGet("/play", async context =>
+                {
+                    var gameStateService = context.RequestServices.GetRequiredService<IGameStateService>();
+
+                    var gameVisualizer = context.RequestServices.GetRequiredService<IGameVisualizer>();
+
+                    var gameState = await gameStateService.InitializeGameAsync(context.RequestAborted);
+
+                    context.Response.Headers.Add("X-GameId", gameState.Id.ToString());
+
+                    context.Response.Headers.ContentType = "text/plain; charset=UTF-8";
+
+                    await context.Response.WriteAsync(gameVisualizer.Draw(gameState.Game), context.RequestAborted);
+                });
+
                 endpoints.MapGet("/draw/{gameId}", async context => 
                 {
                     var gameStateService = context.RequestServices.GetRequiredService<IGameStateService>();
 
                     var gameVisualizer = context.RequestServices.GetRequiredService<IGameVisualizer>();
 
-                    var hashIds = context.RequestServices.GetRequiredService<IHashids>();
-
                     var routeData = context.GetRouteData();
 
-                    if (!routeData.Values.TryGetValue("gameId", out var routeId) || routeId is not string gameHash)
+                    if (
+                        !routeData.Values.TryGetValue("gameId", out var routeId) || 
+                        routeId is not string gameHash ||
+                        !Guid.TryParse(gameHash, out var gameId)
+                    )
                     {
                         context.Response.StatusCode = StatusCodes.Status404NotFound;
 
                         return;
                     }
 
-                    var decoded = hashIds.Decode(gameHash);
-                    
-                    if (decoded.Length != 1)
-                    {
-                        context.Response.StatusCode = StatusCodes.Status400BadRequest;
-
-                        await context.Response.WriteAsync("Malformed game code");
-
-                        return;
-                    }
-
-                    var gameState = await gameStateService.GetLastStateAsync(decoded[0], context.RequestAborted);
+                    var gameState = await gameStateService.GetLastStateAsync(gameId, context.RequestAborted);
 
                     if (gameState == null)
                     {
@@ -91,7 +102,7 @@ namespace LightsOut.Web
 
                     context.Response.Headers.ContentType = "text/plain; charset=UTF-8";
                     
-                    if (gameState.SurrenderedAtUtc.HasValue)
+                    if (gameState.SurrenderedAtUTC.HasValue)
                     {
                         await context.Response.WriteAsync("Game Over! You took too long to solve this puzzle.", context.RequestAborted);
                     }
@@ -99,6 +110,62 @@ namespace LightsOut.Web
                     {
                         await context.Response.WriteAsync(gameVisualizer.Draw(gameState.Game), context.RequestAborted);
                     }
+                });
+
+                endpoints.MapGet("/toggle/{gameId}/{cell}", async context =>
+                {
+                    var gameStateService = context.RequestServices.GetRequiredService<IGameStateService>();
+
+                    var gameVisualizer = context.RequestServices.GetRequiredService<IGameVisualizer>();
+                    
+                    var routeData = context.GetRouteData();
+
+                    var regex = new Regex("([rR][0-9]+)|([cC][0-9]+)");
+
+                    if (
+                        !routeData.Values.TryGetValue("gameId", out var routeId) || 
+                        routeId is not string gameHash ||
+                        !Guid.TryParse(gameHash, out var gameId) ||
+
+                        !routeData.Values.TryGetValue("cell", out var routeCell) || 
+                        routeCell is not string cell ||
+                        !regex.IsMatch(cell)
+                    )
+                    {
+                        context.Response.StatusCode = StatusCodes.Status404NotFound;
+
+                        return;
+                    }
+                    
+                    var matches = regex.Matches(cell);
+
+                    var gameState = await gameStateService.GetLastStateAsync(gameId, context.RequestAborted);
+
+                    if (gameState == null || matches.Count != 2)
+                    {
+                        context.Response.StatusCode = StatusCodes.Status404NotFound;
+
+                        return;
+                    }
+
+                    byte row, column;
+
+                    if (matches[0].Value.StartsWith('r'))
+                    {
+                        row = byte.Parse(matches[0].Value[1..]);
+                        column = byte.Parse(matches[1].Value[1..]);
+                    }
+                    else
+                    {
+                        row = byte.Parse(matches[1].Value[1..]);
+                        column = byte.Parse(matches[0].Value[1..]);
+                    }
+
+                    gameState.Game.ToggleCell(row, column);
+
+                    context.Response.Headers.ContentType = "text/plain; charset=UTF-8";
+
+                    await context.Response.WriteAsync(gameVisualizer.Draw(gameState.Game), context.RequestAborted);
                 });
 
                 endpoints.MapRazorPages();
