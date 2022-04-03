cd01cec75a79895ff24709a074616531d7c4f504
adrianvellamlt <adrian.vella@outlook.com>
2022-04-03 21:45:12 +0200

    HighScore model changes

--- src/lib/GameLogic/Services/Models/HighScore.cs
+++ src/lib/GameLogic/Services/Models/HighScore.cs
@@ -2,14 +2,15 @@ using System;
 
 namespace LightsOut.GameLogic
 {
-    public class HightScore
+    public class HighScore
     {
         public Guid GameStateId { get; set; }
+        public string  Username { get; set; } = string.Empty;
         public byte ComplexityLevel { get; set; }
         public byte NoOfRows { get; set; }
         public byte NoOfColumns { get; set; }
         public byte RemainingLights { get; set; }
-        public string? TimeTakenStr { get; set; }
+        public string TimeTakenStr { get; set; } = "00:00:00";
         public TimeSpan TimeTaken
         {
             get => TimeSpan.Parse(TimeTakenStr);
