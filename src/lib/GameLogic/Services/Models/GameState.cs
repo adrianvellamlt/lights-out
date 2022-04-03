0ddf1387159c9b33df29cc6444e3e666f7a08058
adrianvellamlt <adrian.vella@outlook.com>
2022-04-03 21:45:00 +0200

    Added Username in GameState

--- src/lib/GameLogic/Services/Models/GameState.cs
+++ src/lib/GameLogic/Services/Models/GameState.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace LightsOut.GameLogic
 {
-    public record GameState(Guid Id, ushort GameId, DateTime StartTimeUtc, TimeSpan GameMaxDuration, LightsOut Game)
+    public record GameState(Guid Id, string Username, ushort GameId, DateTime StartTimeUtc, TimeSpan GameMaxDuration, LightsOut Game)
     {
         public DateTime? SurrenderedAtUTC { get; private set; }
         public DateTime? CompletedAtUTC { get; private set; }
